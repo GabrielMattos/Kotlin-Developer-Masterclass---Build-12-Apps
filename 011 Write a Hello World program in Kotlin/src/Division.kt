9aa4cef43261a94f44863d1169581e49b32314bb
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-17 15:20:23 -0300

    026 Kotlin - Division

---
+++ 011 Write a Hello World program in Kotlin/src/Division.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    var firstNumber = 10
+        var secondNumber = 20
+    //var result:Int?
+
+    //result = firstNumber + secondNumber
+
+    println("The result is ${firstNumber / secondNumber.toFloat()}")
+}

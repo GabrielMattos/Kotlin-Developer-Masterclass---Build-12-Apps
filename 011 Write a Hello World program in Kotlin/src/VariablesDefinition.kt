a9321949d74c6dd2306bf1258e4f1971aa33fefd
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-17 11:32:36 -0300

    013 Kotlin Variables

---
+++ 011 Write a Hello World program in Kotlin/src/VariablesDefinition.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    var name = "Gabriel"
+    var lastName = "Sousa"
+    var dogname = "Cachorro"
+
+    println(name + " " + lastName)
+    println(lastName)
+    println(dogname)
+}

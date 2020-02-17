1fc9328f8d7cab7c347b4f5a6ece8f46f91a5b83
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-17 14:55:26 -0300

    021 Kotlin - How to Read Input with readLine Method - App - Store Coversation

---
+++ 011 Write a Hello World program in Kotlin/src/ReadInput.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+
+    println("======================")
+    println(" -> Welcome to our Store <- ")
+    println("======================")
+
+    println("Hello! what can we do for you?")
+    var customerResponse = readLine()
+    println("Yes, indeed. We have fresh bread off the oven! " + "It's your lucky day!")
+
+    var customerSecondResponse = readLine()
+    println("You are very welcome! Have a nice day!")
+
+    println("==================== ==================")
+    println("==================== ==================")
+
+    println("Your conversation with the clerk")
+
+    //println(customerResponse)
+}

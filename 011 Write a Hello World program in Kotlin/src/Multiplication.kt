0c3e3ed403dec963ff9fae94ae334f681e53d3c0
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-17 15:13:36 -0300

    025 Kotlin - Multiplying Numbers

---
+++ 011 Write a Hello World program in Kotlin/src/Multiplication.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    var firstNumber:Int = 10
+    var secondNumber:Int = 20
+    //var result:Int?
+
+    //result = firstNumber + secondNumber
+
+    println("The result is ${firstNumber * secondNumber}")
+}

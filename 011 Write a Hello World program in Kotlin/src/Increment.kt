89c0888a0a85b9d65ba5c510aecd4ed9cb3af6b5
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-17 15:28:23 -0300

    028 Incrementing and Decrementing

---
+++ 011 Write a Hello World program in Kotlin/src/Increment.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    var firstNumber = 10
+    var secondNumber = 20
+
+    var increment = firstNumber + 1
+    var decrement = secondNumber - 1
+
+    println(--firstNumber)
+    println(++secondNumber)
+}

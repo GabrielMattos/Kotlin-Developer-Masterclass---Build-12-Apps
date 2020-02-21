b5fb98d8e010e852d5d66e58fbde3810cacd1637
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-21 08:36:57 -0300

    040 Functions With Parameters

---
+++ 011 Write a Hello World program in Kotlin/src/Functions.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+    add(3, 2)
+    subtract(20, 10)
+}
+
+fun add(num_1:Int, num_2:Int)
+{
+    var sum = num_1 + num_2
+    println(sum)
+}
+
+fun subtract(num_1:Int, num_2:Int)
+{
+    var sum = num_1 - num_2
+    println(sum)
+}

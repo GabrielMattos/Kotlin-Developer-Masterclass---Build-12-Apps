d93eb3018a2a423b124c8d1dcb7809ab5b03d5fa
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-21 11:29:39 -0300

    049 Overloading Constructors

--- 011 Write a Hello World program in Kotlin/src/Animal.kt
+++ 011 Write a Hello World program in Kotlin/src/Animal.kt
@@ -15,7 +15,25 @@ open class Animal()
         println("Object: " + this.color)
         println("Object: " + this.numLegs)
     }
+
+    constructor(name:String, color:String) : this()
+    {
+        this.name = name
+        this.color = color
+
+        println("Third constructor: " + this.name)
+        println("Third constructor: " + this.color)
+    }
+
+    fun showAnimal()
+    {
+        println("Name is: " + this.name)
+        println("Name is: " + this.color)
+        println("Name is: " + this.numLegs)
+    }
 }
+
+
 
 class Lion() : Animal()
 {
@@ -24,10 +42,21 @@ class Lion() : Animal()
 
 fun main(args: Array<String>) {
 
-    var lion = Lion()
+    //var animal = Animal("Animal", "Brown")
+    var animal = Animal()
+    animal.color = "Brown"
+    animal.name = "Lion"
+    animal.numLegs = 4
+
+    var animal2 = Animal("Another animal", "Blue", 6)
+
+    animal.showAnimal()
+    animal2.showAnimal()
+
+    /*var lion = Lion()
     lion.numLegs = 4
 
     println("Num legs " + lion.numLegs)
 
-    //var animal = Animal("Dog", "Brown", 4)
+    //var animal = Animal("Dog", "Brown", 4)*/
 }

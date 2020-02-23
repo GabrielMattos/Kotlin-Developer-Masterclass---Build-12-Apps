81af22c9a312533fdb78de6505936ff7eba3d97b
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-23 03:03:49 -0300

    065 writing to File with User Input and Appending Characters

--- 011 Write a Hello World program in Kotlin/src/WriteToFile.kt
+++ 011 Write a Hello World program in Kotlin/src/WriteToFile.kt
@@ -3,16 +3,24 @@ import java.lang.Exception
 
 fun main(args: Array<String>) {
 
-    var message = "klkwgkjljwkljwnwoiw iuonwuwngklnw lknkwnglknlkenklerlklneer"
-    WriteToFile(message)
+    println("Enter a message")
+    var message = readLine().toString()
+    if(message.equals(""))
+    {
+        println("Please enter a message")
+    } else
+    {
+        WriteToFile(message)
+    }
+
 }
 
 fun WriteToFile(message:String)
 {
     try
     {
-        var writer = FileWriter("message.txt")
-        writer.write(message)
+        var writer = FileWriter("message.txt", true)
+        writer.write(message + "\n")
         writer.close()
     }
 

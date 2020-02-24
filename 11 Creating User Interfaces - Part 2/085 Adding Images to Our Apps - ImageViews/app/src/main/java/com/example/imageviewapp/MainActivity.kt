2d62317b45e2ac2cdbee42e86c959222178d77b5
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-24 10:56:35 -0300

    086 Adding Images to Our Apps - Why we Use Kotlin Extensions Plugin

--- 11 Creating User Interfaces - Part 2/085 Adding Images to Our Apps - ImageViews/app/src/main/java/com/example/imageviewapp/MainActivity.kt
+++ 11 Creating User Interfaces - Part 2/085 Adding Images to Our Apps - ImageViews/app/src/main/java/com/example/imageviewapp/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.imageviewapp
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var catImg = imageView_cat
+        var dogImg = imageView_dog
+
+        catImg.setColorFilter(Color.BLUE)
+        dogImg.setColorFilter(Color.CYAN)
+
+        catImg.setOnClickListener {
+            catImg.clearColorFilter()
+            //catImg.setColorFilter(Color.GREEN, PorterDuff.Mode.DARKEN)
+            catImg.setBackgroundColor(Color.MAGENTA)
+        }
     }
 }

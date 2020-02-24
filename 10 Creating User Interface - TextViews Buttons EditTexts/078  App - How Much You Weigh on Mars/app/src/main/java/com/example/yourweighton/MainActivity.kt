6818bb2e56b496fd7834a5d442037eb38165fcdc
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-23 23:04:41 -0300

    081 App- How Much You Weigh on. Improved Version 2

--- 10 Creating User Interface - TextViews Buttons EditTexts/078  App - How Much You Weigh on Mars/app/src/main/java/com/example/yourweighton/MainActivity.kt
+++ 10 Creating User Interface - TextViews Buttons EditTexts/078  App - How Much You Weigh on Mars/app/src/main/java/com/example/yourweighton/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.yourweighton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.CheckBox
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     val marsGravity:Float = 0.38f
+    val venusGravity:Float = 0.91f
+    val jupiterGravity:Float = 2.34f
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +24,27 @@ class MainActivity : AppCompatActivity() {
             var result = calculateWeight(weight.toString().toDouble())
             textView_result_ID.text = "You weight " + result.toString() + " on Mars"
         }
+
+        checkBox_mars.setOnClickListener(this) //registering our clicklistener
+        checkBox_jupiter.setOnClickListener(this)
+        checkBox_venus.setOnClickListener(this)
     }
 
-    fun calculateWeight(userWeight:Double) : Double
+    private fun calculateWeight(userWeight:Double) : Double
     {
         return userWeight * marsGravity
+    }
+
+    override fun onClick(v: View?) {
+
+        v as CheckBox
+        var isChecked:Boolean = v.isChecked
+
+        when(v.id)
+        {
+            R.id.checkBox_mars -> if(isChecked){Log.d("Setup Mars", "Mars")}
+            R.id.checkBox_jupiter -> if(isChecked){Log.d("Setup Jupiter", "Jupiter")}
+            R.id.checkBox_venus -> if(isChecked){Log.d("Setup Venus", "Venus")}
+        }
     }
 }

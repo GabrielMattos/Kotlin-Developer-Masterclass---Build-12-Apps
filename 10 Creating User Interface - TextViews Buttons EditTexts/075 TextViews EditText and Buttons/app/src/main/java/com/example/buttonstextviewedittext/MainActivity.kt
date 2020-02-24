6039cb5740e1bf6a14636577aabbf23559353a00
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-23 21:05:03 -0300

    076 Enter Name and Show - Final

--- 10 Creating User Interface - TextViews Buttons EditTexts/075 TextViews EditText and Buttons/app/src/main/java/com/example/buttonstextviewedittext/MainActivity.kt
+++ 10 Creating User Interface - TextViews Buttons EditTexts/075 TextViews EditText and Buttons/app/src/main/java/com/example/buttonstextviewedittext/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.buttonstextviewedittext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var text = editText_enterName.text
+        button_showName.setOnClickListener {
+            if(text.isEmpty()) textView_result.text = "Enter name" else textView_result.text = "Welcome " + text
+        }
+
     }
 }

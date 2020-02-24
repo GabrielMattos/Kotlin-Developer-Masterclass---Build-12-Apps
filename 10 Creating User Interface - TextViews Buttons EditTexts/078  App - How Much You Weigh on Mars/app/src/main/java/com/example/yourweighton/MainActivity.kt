20e8c18667cc73f30f18637aaaf3fcedc1bb5228
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-23 22:29:37 -0300

    079 App - How Much You Weigh on Other Planets

--- 10 Creating User Interface - TextViews Buttons EditTexts/078  App - How Much You Weigh on Mars/app/src/main/java/com/example/yourweighton/MainActivity.kt
+++ 10 Creating User Interface - TextViews Buttons EditTexts/078  App - How Much You Weigh on Mars/app/src/main/java/com/example/yourweighton/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.yourweighton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val marsGravity:Float = 0.38f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var weight = editText_enter_ID.text
+
+        button_show_ID.setOnClickListener {
+            var result = calculateWeight(weight.toString().toDouble())
+            textView_result_ID.text = "You weight " + result.toString() + " on Mars"
+        }
+    }
+
+    fun calculateWeight(userWeight:Double) : Double
+    {
+        return userWeight * marsGravity
     }
 }

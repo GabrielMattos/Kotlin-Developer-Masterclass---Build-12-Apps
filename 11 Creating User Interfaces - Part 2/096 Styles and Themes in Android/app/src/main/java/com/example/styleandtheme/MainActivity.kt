bcaba5329230ced3b1e59807e611a5d1e4ac14b1
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-24 14:58:40 -0300

    097 Context in Android and Toast Class

--- 11 Creating User Interfaces - Part 2/096 Styles and Themes in Android/app/src/main/java/com/example/styleandtheme/MainActivity.kt
+++ 11 Creating User Interfaces - Part 2/096 Styles and Themes in Android/app/src/main/java/com/example/styleandtheme/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.styleandtheme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Toast.makeText(getApplication(), "Hello", Toast.LENGTH_LONG).show()
+        Log.v(TAG, getApplicationContext().getString(R.string.app_name))
+        //Toast.makeText(this, "Hello", Toast.LENGTH_LONG).show() //this == class == MainActivity == MainActivity.this
     }
 }

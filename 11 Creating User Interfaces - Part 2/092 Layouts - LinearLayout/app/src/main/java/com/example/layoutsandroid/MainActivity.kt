6bdaa17fe33973c14d3f621bf8b808362c28dd2c
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-25 18:23:09 -0300

    098 Debugging Your Code

--- 11 Creating User Interfaces - Part 2/092 Layouts - LinearLayout/app/src/main/java/com/example/layoutsandroid/MainActivity.kt
+++ 11 Creating User Interfaces - Part 2/092 Layouts - LinearLayout/app/src/main/java/com/example/layoutsandroid/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        add(10, 20)
+    }
+
+    fun add(firstNum:Int, secondNum:Int)
+    {
+         println("Sum is: ${firstNum + secondNum}")
     }
 }

080b5f48d4fdb1bd725fea3ae7986d862d00257e
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-25 18:49:13 -0300

    100 Activity lifecycle

--- 11 Creating User Interfaces - Part 2/092 Layouts - LinearLayout/app/src/main/java/com/example/layoutsandroid/MainActivity.kt
+++ 11 Creating User Interfaces - Part 2/092 Layouts - LinearLayout/app/src/main/java/com/example/layoutsandroid/MainActivity.kt
@@ -2,22 +2,40 @@ package com.example.layoutsandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    //Lifecycle of activities
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        add(10, 20)
-    }
-
-    fun add(firstNum:Int, secondNum:Int)
-    {
-         println("Sum is: ${firstNum + secondNum}")
+        Toast.makeText(this, "onCreate started", Toast.LENGTH_LONG).show()
     }
 
     override fun onPause() {
         super.onPause()
+        Toast.makeText(this, "OnPause started", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+        Toast.makeText(this, "onPostResume started", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy started", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "onStart started", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop started", Toast.LENGTH_LONG).show()
     }
 }

70a010cc5348b3f80ec9ed645805fe7413760d3b
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-02-26 00:13:46 -0300

    103 Passing data back to first Activity

--- 12 Activities and Navigations/101 Navigating from One Activity to Another/app/src/main/java/com/example/shownextactivity/MainActivity.kt
+++ 12 Activities and Navigations/101 Navigating from One Activity to Another/app/src/main/java/com/example/shownextactivity/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.shownextactivity
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_CODE:Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +21,19 @@ class MainActivity : AppCompatActivity() {
             myIntent.putExtra("char", 'G')
             myIntent.putExtra("int", 25)
 
-            startActivity(myIntent)
+            startActivityForResult(myIntent, REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE)
+        {
+            if(resultCode == Activity.RESULT_OK)
+            {
+                var result = data!!.extras!!.get("return").toString()
+                Toast.makeText(this, result, Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
--- 12 Activities and Navigations/101 Navigating from One Activity to Another/app/src/main/java/com/example/shownextactivity/SecondActivity.kt
+++ 12 Activities and Navigations/101 Navigating from One Activity to Another/app/src/main/java/com/example/shownextactivity/SecondActivity.kt
@@ -1,8 +1,10 @@
 package com.example.shownextactivity
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
@@ -19,6 +21,13 @@ class SecondActivity : AppCompatActivity() {
             var int = data.get("int")
 
             Toast.makeText(this, name.toString() + " char: " + char + " Int: " + int, Toast.LENGTH_LONG).show()
+
+            button_goBack_ID.setOnClickListener {
+                val returnIntent = this.intent
+                returnIntent.putExtra("return", "hello test from second activity")
+                setResult(Activity.RESULT_OK, returnIntent)
+                finish()
+            }
 
         }
     }

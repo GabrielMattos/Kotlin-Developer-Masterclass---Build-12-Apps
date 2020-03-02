e368b22185c33f1b93f840d919cb20b26eb1821e
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-03-02 01:09:56 -0300

    141 App - Recipe Finder - Parsing Recipies

--- 15 Connecting to the World - Connecting and Consuming JSON data from the Internet/RecipeFinderApp/app/src/main/java/com/example/recipefindermyapp/activities/MainActivity.kt
+++ 15 Connecting to the World - Connecting and Consuming JSON data from the Internet/RecipeFinderApp/app/src/main/java/com/example/recipefindermyapp/activities/MainActivity.kt
@@ -1,13 +1,19 @@
  package com.example.recipefindermyapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.recipefindermyapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 
  class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_search_ID.setOnClickListener {
+            startActivity(Intent(this, RecipeList::class.java))
+        }
     }
 }

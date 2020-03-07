b140f2c088a764a6871bbebabdb8f75283480d06
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-03-07 20:32:34 -0300

    145 Firebase - Read Values from Firebase Database

--- 16 Firebase - Cloud Based Application Development/144 Firebase - Introduction to Cloud Based Services - Setup/app/src/main/java/com/example/introkotlinfirebase/MainActivity.kt
+++ 16 Firebase - Cloud Based Application Development/144 Firebase - Introduction to Cloud Based Services - Setup/app/src/main/java/com/example/introkotlinfirebase/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.introkotlinfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +15,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var firebaseDatabas = FirebaseDatabase.getInstance()
-        var databaseRef = firebaseDatabas.reference
+        var databaseRef = firebaseDatabas.getReference("messages")
 
         databaseRef.setValue("Hello there!")
+
+        //Read from our DB
+        databaseRef.addValueEventListener(object:ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+
+                Log.d("Error: ", error!!.toString())
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                var value = dataSnapshot!!.value
+                Log.d("VALUE: ", value.toString())
+            }
+        })
     }
 }

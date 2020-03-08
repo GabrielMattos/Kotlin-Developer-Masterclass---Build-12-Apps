c726a6b09cf85d5005c94e707893c5c13c28446e
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-03-07 22:52:32 -0300

    147 Firebase - Login User With Email and Password

--- 16 Firebase - Cloud Based Application Development/144 Firebase - Introduction to Cloud Based Services - Setup/app/src/main/java/com/example/introkotlinfirebase/MainActivity.kt
+++ 16 Firebase - Cloud Based Application Development/144 Firebase - Introduction to Cloud Based Services - Setup/app/src/main/java/com/example/introkotlinfirebase/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.introkotlinfirebase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
+
+    private var mAuth:FirebaseAuth? = null
+    private var currentUser:FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,24 +25,60 @@ class MainActivity : AppCompatActivity() {
         var firebaseDatabas = FirebaseDatabase.getInstance()
         var databaseRef = firebaseDatabas.getReference("messages").push()
 
-        var employee = Employee("James", "Android Developer", "123 Street", 25)
+        mAuth = FirebaseAuth.getInstance()
 
-        databaseRef.setValue(employee)
+        //Sign existing user in
+        mAuth!!.signInWithEmailAndPassword("paulo@me.com", "password").addOnCompleteListener {
+            task:Task<AuthResult> ->
+            if(task.isSuccessful)
+            {
+                //Sign in was successful
+                Toast.makeText(this, "Sign in was successful", Toast.LENGTH_LONG).show()
+            }
+
+            else
+            {
+                //Not sucessful
+                Toast.makeText(this, "Sign in was UNsuccessful", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        /*var employee = Employee("James", "Android Developer", "123 Street", 25)
+
+
+        //databaseRef.setValue(employee)
 
         //Read from our DB
         databaseRef.addValueEventListener(object:ValueEventListener {
             override fun onCancelled(error: DatabaseError) {
 
-                Log.d("Error: ", error!!.toString())
+                //Log.d("Error: ", error!!.toString())
             }
 
             override fun onDataChange(dataSnapshot: DataSnapshot) {
 
-                var value = dataSnapshot!!.value as HashMap<String, Any>
-                Log.d("VALUE: ", value.get("name").toString())
+                //var value = dataSnapshot!!.value as HashMap<String, Any>
+                //Log.d("VALUE: ", value.get("name").toString())
             }
-        })
+        })*/
     }
+
+    override fun onStart()
+    {
+        super.onStart()
+        currentUser = mAuth!!.currentUser
+        if(currentUser != null)
+        {
+            Toast.makeText(this, "User Is logged in", Toast.LENGTH_LONG).show()
+        }
+
+        else
+        {
+            Toast.makeText(this, "User Is logged out", Toast.LENGTH_LONG).show()
+        }
+        //call a function to update the userInterface with current user
+    }
+
 
     data class Employee(var name:String, var position:String, var homeAdress:String, var age:Int)
     {

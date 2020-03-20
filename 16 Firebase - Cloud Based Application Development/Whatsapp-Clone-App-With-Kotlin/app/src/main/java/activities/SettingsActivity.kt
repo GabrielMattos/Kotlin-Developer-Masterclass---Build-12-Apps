19856185904cec2b64fd30f24f5499cfb4a28ccc
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-03-20 17:04:48 -0300

    161 WhatsApp Clone Chat App - Settings Activity - Part 11

--- 16 Firebase - Cloud Based Application Development/Whatsapp-Clone-App-With-Kotlin/app/src/main/java/activities/SettingsActivity.kt
+++ 16 Firebase - Cloud Based Application Development/Whatsapp-Clone-App-With-Kotlin/app/src/main/java/activities/SettingsActivity.kt
@@ -1,13 +1,50 @@
 package activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mychatapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
+
+    var mDatabase:DatabaseReference? = null
+    var mCurrentUser:FirebaseUser? = null
+    var mStorageRef:StorageReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        mCurrentUser = FirebaseAuth.getInstance().currentUser
+        var userId = mCurrentUser!!.uid
+        mDatabase = FirebaseDatabase.getInstance().reference.child("Users").child(userId)
+
+        mDatabase!!.addValueEventListener(object:ValueEventListener{
+
+            override fun onCancelled(databaseErrorSnapshot: DatabaseError) {
+
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                var displayName = dataSnapshot!!.child("display_name").value
+                var image = dataSnapshot!!.child("image").value
+                var userStatus = dataSnapshot!!.child("status").value
+                var thumbnail = dataSnapshot!!.child("thumb_image").value
+
+                textView_displayName_ID.text = displayName.toString()
+                textView_status_ID.text = userStatus.toString()
+            }
+        })
+
+        button_changeStatus_ID.setOnClickListener {
+            var intent = Intent(this, StatusActivity::class.java)
+            intent.putExtra("status", textView_status_ID.text.toString().trim())
+            startActivity(intent)
+        }
     }
 }
---
+++ 16 Firebase - Cloud Based Application Development/Whatsapp-Clone-App-With-Kotlin/app/src/main/java/activities/StatusActivity.kt
@@ -0,0 +1,13 @@
+package activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mychatapp.R
+
+class StatusActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_status)
+    }
+}

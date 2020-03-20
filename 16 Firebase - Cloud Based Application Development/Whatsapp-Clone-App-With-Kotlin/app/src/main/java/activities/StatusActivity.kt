95f32e33772a30eee2ef3b4f34891fdd406452fc
GABRIEL MATTOS DA SILVA SOUSA <gabriel.mattos17@hotmail.com>
2020-03-20 20:14:43 -0300

    162 WhatsApp Clone Chat App - Status Activity - Part 12

--- 16 Firebase - Cloud Based Application Development/Whatsapp-Clone-App-With-Kotlin/app/src/main/java/activities/StatusActivity.kt
+++ 16 Firebase - Cloud Based Application Development/Whatsapp-Clone-App-With-Kotlin/app/src/main/java/activities/StatusActivity.kt
@@ -3,11 +3,34 @@ package activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mychatapp.R
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import kotlinx.android.synthetic.main.activity_status.*
 
 class StatusActivity : AppCompatActivity() {
+
+    var mDatabase: DatabaseReference? = null
+    var mCurrentUser: FirebaseUser? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_status)
+
+        supportActionBar!!.title = "Status"
+
+        if(intent.extras != null)
+        {
+            var oldStatus = intent.extras!!.get("status")
+            editText_statusUpdate_ID.setText(oldStatus.toString())
+        }
+
+        if(intent.extras?.equals(null)!!)
+        {
+            editText_statusUpdate_ID.setText("Enter Your New Status")
+        }
+
+        editText_statusUpdate_ID.setOnClickListener {
+
+        }
     }
 }
